f2f9faa958948a3d7a2f0e024d6ccecd4e3b15e0
João Vitor <joaovitorgomesdefaria@gmail.com>
2025-09-17 11:25:20 -0300

    começando a aprender

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-gitimport { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  console.log('Oi');
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Olá, Mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+        reprehenderit placeat autem cupiditate eaque deserunt iste quibusdam
+        enim itaque. Alias odio assumenda eaque ex consectetur excepturi quasi
+        quas? Illo, quasi.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+// <> e </> funcionam como uma div, em react não pode existir mais de um 
+// elemento filho solto ex.: h1 e p precisam ser envelopados por div ou <> </>
+// <> </> são chamados de react fragment é usados qunado não se quer usar um elemento pai (ex.: div)
+// um componente no react é basicamente uma função (function), bem parecido com js   
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <App/>
   </StrictMode>,
 )
+
+// dentro de StrictMode eu posso por exemplo criar um html completo, mas não é uma boa prática pq o intuito é usar os componentes do react
+
+// esses .tsx são typescript junto com jsx tsx está dentro dos arquivos do react

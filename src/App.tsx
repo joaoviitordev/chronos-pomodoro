33a246262037b0d1443207466e1625d24afec944
João Vitor <joaovitorgomesdefaria@gmail.com>
2025-09-18 21:08:49 -0300

    ajustei o contador

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -14,6 +15,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );

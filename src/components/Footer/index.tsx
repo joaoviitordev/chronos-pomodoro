e50002e1802b68beefdfad98a17bb11fa1c0e170
João Vitor <joaovitorgomesdefaria@gmail.com>
2025-09-19 16:57:11 -0300

    footer feito

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,10 @@ import styles from './styles.module.css'
 export function Footer () {
   return (
     <>
-    <a className={styles.footer} href="#">Entenda a técnica pomodoro 🍅</a>
-    <a className={styles.footer} href="https://github.com/joaoviitordev/chronos-pomodoro" target='_blank'>Repositório desse projeto!</a>
+    <footer>
+      <a className={styles.footer} href="#">Entenda a técnica pomodoro 🍅</a>
+      <a className={styles.footer} href="https://github.com/joaoviitordev/chronos-pomodoro" target='_blank'>Repositório desse projeto!</a>
+    </footer>
     </>
   );
 }
